d3476639ddc698722efbdc75ec6ef14dbf1feacb
jjeaby <jjeaby@gmail.com>
2018-09-12 01:15:44 +0900

    page structure modify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'About',
+      component: About
     }
+
   ]
 })
