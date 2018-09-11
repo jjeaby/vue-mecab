962a1a1c896d6e7d756c849c2ccdc09279ca321c
lee yong jin <jjeaby@gmail.com>
2018-09-12 00:20:57 +0900

    modify host

---
+++ debug.sh
@@ -0,0 +1,1 @@
+FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
+#FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
+FLASK_APP=run.py flask run --port 18080 --host 0.0.0.0
