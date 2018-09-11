75ba7575f1d75b141823b8947130941120b3ebe2
lee yong jin <jjeaby@gmail.com>
2018-09-12 00:13:55 +0900

    host change 0.0.0.0

--- run.py
+++ run.py
@@ -19,11 +19,11 @@ def random_number():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-#    if app.debug:
-#        return requests.get('http://localhost:8080/{}'.format(path)).text
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=18080)
 
 
