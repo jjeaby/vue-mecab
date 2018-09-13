5a57f529f29aaab2b0836cd99783a3b13710a931
jjeaby <jjeaby@gmail.com>
2018-09-14 01:53:20 +0900

    revert run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
 #FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
+cd frontend
+npm install
+npm run build
+cd ..
 FLASK_APP=run.py flask run --port 18080 --host 0.0.0.0
