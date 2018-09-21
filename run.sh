c5f59fe3e3f8ffbc96b35e8d295f99e71ba1e6ae
jjeaby <jjeaby@gmail.com>
2018-09-22 01:26:33 +0900

    run.sh 에서 디버그 모드로 실행하도록 수정

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
-FLASK_APP=run.py flask run --port 18080 --host 0.0.0.0
+#FLASK_APP=run.py flask run --port 18080 --host 0.0.0.0
+FLASK_DEBUG=1 python3 run.py
