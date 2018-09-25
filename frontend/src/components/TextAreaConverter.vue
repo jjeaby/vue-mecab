6b9a9e44a0705ebdcc5c5d456045e9e8089db079
jjeaby <jjeaby@gmail.com>
2018-09-25 22:23:19 +0900

    dynamic withd add

--- frontend/src/components/TextAreaConverter.vue
+++ frontend/src/components/TextAreaConverter.vue
@@ -2,19 +2,133 @@
     <div class="TextAreaConverter">
         <h1>{{ title }}</h1>
 
+       <table class="tbl">
+            <caption>Mecab Pos Tagger(형태소 분석)</caption>
+            <colgroup>
+                <col style="width: 300px;">
+                <col style="width: 80px;">
+                <col style="width: 300px;">
+            </colgroup>
 
+            <tr>
+                <th scope="col" >
+                    <textarea v-model="srcText" placeholder="Input Text"
+                              cols="48" rows="20"></textarea>
+                </th>
+                <th scope="col">
+                    <button @click="mecabPos()">분석</button>
+                    <p/>
+                    <button @click="mecabPosReset()">리셋</button>
+                </th>
+                <th scope="col" >
+                    <textarea v-model="tgtText" readonly placeholder="Output Text"
+                              cols="48" rows="20"></textarea>
+                </th>
+            </tr>
+        </table>
     </div>
 </template>
 
+<style scoped lang="scss">
+    .TextAreaConverter {
+        padding: 1px 1px 1px 1px;
+        justify-content: center;
+    }
+
+    .title {
+        font-family: "Roboto Slab", "ff-tisa-web-pro", "Georgia", Arial, sans-serif;
+        font-size: x-large;
+        font-style: normal;
+    }
+
+    .tbl {
+        width: 100%;
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    .tbl caption {
+        overflow: hidden;
+        position: absolute;
+        width: 0;
+        height: 0;
+        line-height: 0;
+        text-indent: -9999px
+    }
+
+    .tbl th {
+        padding: 1px 1px 1px 1px;
+    }
+
+    .tbl td {
+        padding: 1px 1px 1px 1px;
+    }
+
+    @media all and (max-width: 900px) {
+        .tbl,
+        .tbl thead,
+        .tbl tbody,
+        .tbl tr,
+        .tbl th,
+        .tbl td {
+            display: block
+        }
+
+    }
+
+</style>
+
 <script>
+import axios from 'axios';
+
 export default {
     name: 'TextAreaConverter',
     props: {
         title: String,
     },
+    data() {
+        return {
+            srcText: '',
+            tgtText: '',
+            error: [],
+        };
+    },
+    created() {
+        // 뷰가 생성되고 데이터가 이미 감시 되고 있을 때 데이터를 가져온다.
+        // this.mecabPos();
+    },
+    watch: {
+        // 라우트가 변경되면 메소드를 다시 호출됩니다.
+        // '$route': 'fetchData'
+        // 'srcText': 'mecabPos'
+    },
+    methods: {
+        // fetchData() {
+        //     this.tgtText = 'adsf';
+        // }
+        mecabPos() {
+            // this.tgtText = 'aaaa';
+            const requestData = {};
+            requestData.srcText = this.srcText;
+
+            // axios.post('http://mecab.jjeaby.ml/api/mecabpos', requestData)
+            axios.post('http://localhost:18080/api/mecabpos', requestData)
+                .then((response) => {
+                    // JSON responses are automatically parsed.
+                    const responseData = response.data;
+                    const responseDataJson = JSON.stringify(responseData);
+                    this.tgtText = JSON.stringify(JSON.parse(responseDataJson), null, 2);
+                })
+                .catch((error) => {
+                    console.log('errorType', typeof error);
+                    console.log('error', Object.assign({}, error));
+                });
+        },
+        mecabPosReset() {
+            this.tgtText = '';
+            this.srcText = '';
+        },
+    },
 };
+
 </script>
-
-<style scoped lang="scss">
-
-</style>
--- frontend/src/views/mecabpostagger.vue
+++ frontend/src/views/mecabpostagger.vue
@@ -1,29 +1,10 @@
 <template>
     <div class="mecab">
         <h1 class="title">Mecab Pos Tagger(형태소 분석)</h1>
-        <table class="tbl">
-            <caption>Mecab Pos Tagger(형태소 분석)</caption>
-            <colgroup>
-                <col style="width: 400px;">
-                <col style="width: 80px;">
-                <col style="width: 400px;">
-            </colgroup>
 
-            <tr>
-                <th scope="col">
-                    <textarea v-model="srcText" placeholder="Input Text" cols="40" rows="20"></textarea>
-                </th>
-                <th scope="col">
-                    <button @click="mecabPos()">분석</button>
-                    <p/>
-                    <button @click="mecabPosReset()">리셋</button>
-                </th>
-                <th scope="col">
-                    <textarea v-model="tgtText" readonly placeholder="Output Text"
-                              cols="40" rows="20"></textarea>
-                </th>
-            </tr>
-        </table>
+        <TextAreaConverter msg="Welcome to Your Vue.js App"/>
+
+
     </div>
 
 
@@ -35,99 +16,17 @@
         justify-content: center;
     }
 
-    .title {
-        font-family: "Roboto Slab", "ff-tisa-web-pro", "Georgia", Arial, sans-serif;
-        font-size: x-large;
-        font-style: normal;
-    }
-
-    .tbl {
-        width: 100%;
-        border-collapse: collapse;
-        border-spacing: 0;
-    }
-
-    .tbl caption {
-        overflow: hidden;
-        position: absolute;
-        width: 0;
-        height: 0;
-        line-height: 0;
-        text-indent: -9999px
-    }
-
-    .tbl th {
-        padding: 10px 0;
-    }
-
-    .tbl td {
-        padding: 10px 0 10px 10px;
-    }
-
-    @media all and (max-width: 880px) {
-        .tbl,
-        .tbl thead,
-        .tbl tbody,
-        .tbl tr,
-        .tbl th,
-        .tbl td {
-            display: block
-        }
-
-    }
-
 </style>
 
 <script>
-import axios from 'axios';
+import TextAreaConverter from '@/components/TextAreaConverter.vue';
 
 export default {
 
     name: 'mecabpostagger',
-
-    data() {
-        return {
-            srcText: '',
-            tgtText: '',
-            error: [],
-        };
-    },
-    created() {
-        // 뷰가 생성되고 데이터가 이미 감시 되고 있을 때 데이터를 가져온다.
-        // this.mecabPos();
-    },
-    watch: {
-        // 라우트가 변경되면 메소드를 다시 호출됩니다.
-        // '$route': 'fetchData'
-        // 'srcText': 'mecabPos'
-    },
-    methods: {
-        // fetchData() {
-        //     this.tgtText = 'adsf';
-        // }
-        mecabPos() {
-            // this.tgtText = 'aaaa';
-            const requestData = {};
-            requestData.srcText = this.srcText;
-
-            // axios.post('http://mecab.jjeaby.ml/api/mecabpos', requestData)
-            axios.post('http://localhost:18080/api/mecabpos', requestData)
-                .then((response) => {
-                    // JSON responses are automatically parsed.
-                    const responseData = response.data;
-                    const responseDataJson = JSON.stringify(responseData);
-                    this.tgtText = JSON.stringify(JSON.parse(responseDataJson), null, 2);
-                })
-                .catch((error) => {
-                    console.log('errorType', typeof error);
-                    console.log('error', Object.assign({}, error));
-                });
-        },
-        mecabPosReset() {
-            this.tgtText = '';
-            this.srcText = '';
-        },
-    },
+    components: {
+        TextAreaConverter,
+    }
 };
 
 
