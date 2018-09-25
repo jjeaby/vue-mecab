12bcf74be004f2b7d8d8c5401d041c4857826542
lee yong jin <jjeaby@gmail.com>
2018-09-26 01:36:27 +0900

    run.sh 수정

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #FLASK_APP=run.py FLASK_DEBUG=1 flask run --port 18080 --host 0.0.0.0
 #FLASK_APP=run.py flask run --port 18080 --host 0.0.0.0
+killall python3
+cd frontend 
+npm run build
+cd ..
 FLASK_DEBUG=1 python3 run.py
